193f48a65e7869ed4f81f5d6ad551ff5f6a13a1a
stoychevl <stoychevl@vmware.com>
2024-01-03 17:47:20 +0200

    I made this example less boring by adding some JS

---
+++ scripts/shake.js
@@ -0,0 +1,40 @@
+function shakeElementsBySelector(selector) {
+    var elements = document.querySelectorAll(selector);
+    elements.forEach(function (element) {
+        var shakeAnimation = element.animate([
+            { transform: 'translateX(20px)' },
+            { transform: 'translateX(-20px)' },
+            { transform: 'translateY(20px)' },
+            { transform: 'translateY(-20px)' },
+        ], {
+            duration: 100,
+            iterations: Infinity
+        });
+        setTimeout(() => shakeAnimation.cancel(), 1000);
+    });
+}
+
+function shakeBody() {
+    console.log("Shake that body!");
+    shakeElementsBySelector("body");
+}
+
+function fallApart() {
+    console.log("Oh Nooo!!!");
+
+    var elements = document.querySelectorAll("div");
+    elements.forEach(function (element) {
+        // Generate a random angle
+        var angle = Math.random() * 2 * Math.PI;
+
+        // Calculate the new position
+        var x = Math.cos(angle) * 100;
+        var y = Math.sin(angle) * 100;
+
+        // Apply the transform to the element
+        element.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+        // Add a transition to animate the transform
+        element.style.transition = 'transform 2s';
+    });
+}
